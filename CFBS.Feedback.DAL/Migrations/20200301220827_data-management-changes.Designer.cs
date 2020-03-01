269a564b4a5ef7cfe95da263d8430fec64854444
Lewis carter <lewiscarter998@gmail.com>
2020-03-01 22:10:08 +0000

    Database updated

--- CFBS.Feedback.DAL/Migrations/20200301220827_data-management-changes.Designer.cs
+++ CFBS.Feedback.DAL/Migrations/20200301220827_data-management-changes.Designer.cs
@@ -4,14 +4,16 @@ using CFBS.Feedback.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CFBS.Feedback.DAL.Migrations
 {
     [DbContext(typeof(FeedbackContext))]
-    partial class FeedbackContextModelSnapshot : ModelSnapshot
+    [Migration("20200301220827_data-management-changes")]
+    partial class datamanagementchanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CFBS.Feedback.DAL/Migrations/20200301220827_data-management-changes.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CFBS.Feedback.DAL.Migrations
+{
+    public partial class datamanagementchanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ActiveQuestions",
+                table: "ActiveQuestions");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "ImageAnswers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ID",
+                table: "ActiveQuestions",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ActiveQuestions",
+                table: "ActiveQuestions",
+                column: "ID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActiveQuestions_LocationID",
+                table: "ActiveQuestions",
+                column: "LocationID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ActiveQuestions",
+                table: "ActiveQuestions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ActiveQuestions_LocationID",
+                table: "ActiveQuestions");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "ImageAnswers");
+
+            migrationBuilder.DropColumn(
+                name: "ID",
+                table: "ActiveQuestions");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ActiveQuestions",
+                table: "ActiveQuestions",
+                columns: new[] { "LocationID", "QuestionID" });
+        }
+    }
+}
